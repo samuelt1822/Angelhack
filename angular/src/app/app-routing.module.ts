afcc19a6afa601676dde3dd375bb3b77f00fa558
samuelt1822 <samuel.te@husky.neu.edu>
2019-07-13 17:46:26 -0700

    Updated angular

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {UserComponent} from "./components/user/user.component";
+import {LandingPageComponent} from "./components/landing-page/landing-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LandingPageComponent},
+  {path: 'account', component: UserComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { UserComponent } from './components/user/user.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [], //add providers for all client side services for components
   bootstrap: [AppComponent]
 })
 export class AppModule { }
